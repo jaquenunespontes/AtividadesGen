b4f3f4d987afde268577206c9f2cd84e9234448f
Jaqueline Pontes <120497663+jaquenunespontes@users.noreply.github.com>
2023-01-03 15:57:08 -0300

    Add files via upload

---
+++ Exercicio1.java
@@ -0,0 +1,55 @@
+package ExercicioRepeticao;
+
+public class Exercicio1 {
+	package DesenvolvimentoJava;
+
+	import java.util.Scanner;
+
+	public class LacoFor 
+	{
+
+		public static void main(String[] args) {
+			
+			
+	Scanner leia = new Scanner(System.in);
+			
+			
+	float n1,n2,n3,media,somaMedia=0,mediaGeral;
+			
+	int x;
+			
+			
+	for(x=1;x<=4;x++) {
+				
+	System.out.println("\nEntre com a 1º: ");
+				
+	n1 = leia.nextFloat();
+				
+	System.out.println("\nentre com a 2º nota: ");
+				
+	n2 = leia.nextFloat();
+				
+	System.out.println("\nEntre com a 3º nota: ");
+				
+	n3 = leia.nextFloat();
+				
+				
+	media = (n1+n2+n3)/3;
+				
+	System.out.printf("\nMedia: %3.2f",media);
+				
+	somaMedia += media; // somaMedia = somaMedia + media
+			
+	}
+			mediaGeral = somaMedia / 4;
+			
+	System.out.println("\nMédia Geral: "+mediaGeral);
+
+		
+	}
+
+
+	}
+
+
+}

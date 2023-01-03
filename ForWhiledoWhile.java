9b3dbeee40313d6d90fb69881be72c398d6420c1
Jaqueline Pontes <120497663+jaquenunespontes@users.noreply.github.com>
2023-01-03 16:46:46 -0300

    Add files via upload

--- ForWhiledoWhile.java
+++ ForWhiledoWhile.java
@@ -87,3 +87,28 @@ public class LacoFor {
 	}
 
 }
+
+public class Exercicio3While {
+    public static void main(String args[])
+    {
+        Scanner ler = new Scanner(System.in);
+        int menor21=0, maior50=0, idade=0;
+
+        while(idade >= 0)
+        {
+            System.out.printf("Entre com a idade: ");
+            idade = ler.nextInt();
+
+            if(idade <21 && idade > 0)
+            {
+                menor21 += 1;
+            }
+            if(idade > 50)
+            {
+                maior50 += 1;
+            }
+        }
+        System.out.println("Pessoas menores de 21 anos: "+menor21);
+        System.out.println("Pessoas maiores de 50 anos: "+maior50);
+    }
+}
